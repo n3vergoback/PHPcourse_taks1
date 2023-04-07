1c7a57db69911d281ce4c8ab38d2129dc613c1a6
n3vergoback <ngbcs@yandex.ru>
2023-04-07 15:42:45 +0400

    controllers added

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,4 @@
 <?php
+require_once 'libraries/core.php';
+require_once 'libraries/db.php';
+require_once 'libraries/controller.php';
---
+++ app/libraries/Core.php
@@ -0,0 +1,24 @@
+<?php
+class Core {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $parameters = [];
+
+    public function __construct(){
+        $url = $this->getUrl();
+        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
+    }
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
